442d0095852230fb477b050598360a4a6b0a7728
yakubalkis <alkis.yakub@gmail.com>
2023-03-25 22:15:25 +0300

    Added PUT http method for updating the customer.

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,4 +60,21 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
+	// add mapping for PUT /customers - update existing customer
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
 }
+
+
+
+
+
+
+
