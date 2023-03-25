be61d3725c5084035728dafa185bf754f278fd7c
yakubalkis <alkis.yakub@gmail.com>
2023-03-25 14:57:10 +0300

    added mapping for getting customer by id.

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,14 @@ public class CustomerRestController {
 	public List<Customer> getCustomers() {
 		
 		return customerService.getCustomers();
+	}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
 	}
 	
 }
