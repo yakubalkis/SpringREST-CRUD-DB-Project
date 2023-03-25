3b51cdc149f57ee894049097e7f015e828b423a6
yakubalkis <alkis.yakub@gmail.com>
2023-03-25 21:27:14 +0300

    Added POST http method for creating new customer.

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +22,15 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerService customerService;
 	
-	// add mapping for Get /customers
+	// add mapping for GET /customers
 	
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {
 		
 		return customerService.getCustomers();
 	}
+	
+	// add mapping for GET /customers/{customerId}
 	
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
@@ -36,6 +40,21 @@ public class CustomerRestController {
 		if(theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - "+ customerId);
 		}
+		
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customers - add new customer
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update		
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
 	}
